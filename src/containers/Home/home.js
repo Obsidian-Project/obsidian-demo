8227efcc3f4758a343405cbbb9faee25841c42a5
Edson Alcala <edson_alcala@msn.com>
2017-12-23 15:13:56 +0000

    added home page[

--- src/containers/Home/home.js
+++ src/containers/Home/home.js
@@ -1,14 +1,30 @@
 
 import React from 'react';
 import styles from './home.css';
+import { Link } from 'react-router-dom';
+import governments from '../Governments/governments';
+import { Header } from 'semantic-ui-react';
 
 class Home extends React.Component {
     constructor() {
         super();
     }
 
-    render(){
-        return <h1>Home Page</h1>
+    render() {
+        return <div className="home">
+            <Header textAlign="center" as='h1'>
+                <Header.Content> 
+                    Obsidian
+                    <Header.Subheader>
+                        Giving the tools to farmers around the world
+                    </Header.Subheader>
+                </Header.Content>
+            </Header>
+            <div className="entry-links">
+                <Link className="entry-link government" to="/governments">Governments</Link>
+                <Link className="entry-link companies" to="/companies">Companies</Link>
+            </div>
+        </div>
     }
 }
 
